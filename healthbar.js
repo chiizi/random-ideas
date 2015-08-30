e13cc546aca473a9bd130d3fe87c84f5787665eb
chiizi <m33pfish@gmail.com>
2015-08-30 14:50:57 -0500

    Update healthbar.js

--- healthbar.js
+++ healthbar.js
@@ -2,4 +2,6 @@ var changeHealth = function(amount, time) {
   var canvas = document.getElementById("healthbar");
   canvas.width = 200;
   canvas.height = 8;
+  
+  var ctx = canvas.getContext("2d");
 };
