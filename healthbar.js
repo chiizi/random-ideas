7ce001e4708cfc00ce0d41352c1eac4cc84eff59
chiizi <m33pfish@gmail.com>
2015-08-30 20:30:01 -0500

    Add kill switch

--- healthbar.js
+++ healthbar.js
@@ -1,7 +1,13 @@
 var health = 0;
 var healthmax = 100;
+var healthbarKillSwitch;
 
 var changeHealth = (function() {
+  var STOP = false;
+  healthbarKillSwitch = function() {
+    return STOP = !STOP;
+  };
+  
   var canvas = document.getElementById("healthbar");
   canvas.width = 200;
   canvas.height = 8;
@@ -9,12 +15,14 @@ var changeHealth = (function() {
   var ctx = canvas.getContext("2d");
   
   (function() {
+    if (STOP) return;
     var a = document.createElement("code");
     a.id = "debug";
     document.body.appendChild(a);
   })();
   
   var actual = function(amount, count) {
+    if (STOP) return;
     document.getElementById("debug").innerHTML = Array.prototype.slice.call(arguments, 0).toString().replace(",", ", ");
     if (count == 0 || health >= healthmax) {
       return;
@@ -24,12 +32,15 @@ var changeHealth = (function() {
     ctx.fillStyle = "#" + Math.round((100 - health - amount) * 2.56).toString(16) + Math.round((health + amount) * 2.56).toString(16) + "00";
     ctx.fillRect(0, 0, health / healthmax * canvas.width, canvas.height);
     requestAnimationFrame(function() {
+      if (STOP) return;
       actual(amount, count + (count < 0 ? 1 : -1));
     });
   };
   
   return function(amount, time) {
+    if (STOP) return;
     requestAnimationFrame(function() {
+      if (STOP) return;
       actual(amount / (time * 60), time * 60);
     });
   };
